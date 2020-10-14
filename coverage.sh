77e893b41f68f4a14fffc61c849caf3f9e55948a
Viktor Leis <viktor.leis@uni-jena.de>
2020-10-14 10:32:26 +0200

    report too

--- coverage.sh
+++ coverage.sh
@@ -4,3 +4,4 @@ clang++ -fprofile-instr-generate -fcoverage-mapping -O3 btree2020.cpp -o cov
 LLVM_PROFILE_FILE="cov.profraw" SHUF=1 INT=10e6 LONG1=900 LONG2=900 FILE=urlsuniqshufmedium ./cov
 llvm-profdata merge -sparse cov.profraw -o cov.profdata
 llvm-cov show ./cov -instr-profile=cov.profdata
+llvm-cov report ./cov -instr-profile=cov.profdata
