453b8e3ff16748418c75f0852ee058a90ded4299
Viktor Leis <viktor.leis@uni-jena.de>
2020-10-16 16:57:17 +0200

    minor

--- btree2020.hpp
+++ btree2020.hpp
@@ -3,21 +3,20 @@
 struct BTreeNode;
 
 struct BTree {
+   private:
+
    BTreeNode* root;
 
+   void splitNode(BTreeNode* node, BTreeNode* parent, uint8_t* key, unsigned keyLength, unsigned payloadLength);
+   void ensureSpace(BTreeNode* toSplit, uint8_t* key, unsigned keyLength, unsigned payloadLength);
+
    public:
+
    BTree();
    ~BTree();
 
    uint8_t* lookup(uint8_t* key, unsigned keyLength, unsigned& payloadSizeOut);
-
    bool lookup(uint8_t* key, unsigned keyLength);
-
    void insert(uint8_t* key, unsigned keyLength, uint8_t* payload, unsigned payloadLength);
-
    bool remove(uint8_t* key, unsigned keyLength);
-
-   private:
-   void splitNode(BTreeNode* node, BTreeNode* parent, uint8_t* key, unsigned keyLength, unsigned payloadLength);
-   void ensureSpace(BTreeNode* toSplit, uint8_t* key, unsigned keyLength, unsigned payloadLength);
 };
