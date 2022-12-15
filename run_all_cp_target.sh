1e61f72501c77481c9ed5c1e43e14fc4fa9630de
Marcus Mueller <marcus.mueller@fau.de>
2022-12-15 19:00:43 +0100

    update run script

--- run_all_cp_target.sh
+++ run_all_cp_target.sh
@@ -1,11 +1,14 @@
 set -e
 
-OUT="$HOME/common_out.csv"
+OUT="$HOME/common_out.out"
 touch $OUT
 
 cd ~/cp-target
 
 find . -name 'btree-*' -exec chmod u+x {}  \;
 
+for i in {1..40}
+do
 OP_COUNT=1e9 FILE="$HOME/data/urls" find . -name 'btree-*' -exec {} >> $OUT \;
 OP_COUNT=1e9 INT=2e7 find . -name 'btree-*' -exec {} >> $OUT \;
+done
