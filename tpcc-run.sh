42526e8f662df4f82c9bbb274e6c2c35ba4f6923
Marcus Mueller <marcus.mueller@fau.de>
2022-12-20 23:38:51 +0100

    update run scripts

--- tpcc-run.sh
+++ tpcc-run.sh
@@ -1,4 +1,6 @@
 cd tpcc
 make
+rsync tpcc.elf zen4-students:
+ssh zen4-students 'env RUNFOR='$RUNFOR' ./tpcc.elf 1 '$WAREHOUSES &
 rsync tpcc.elf cascade-01:
-ssh -o SendEnv=RUNFOR cascade-01 ./tpcc.elf 1 $WAREHOUSES
+ssh cascade-01 'env RUNFOR='$RUNFOR' ./tpcc.elf 1 '$WAREHOUSES &
