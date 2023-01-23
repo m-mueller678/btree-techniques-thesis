9d8bb77a6f79d787a7f91c8746a1d7da426c81d6
Marcus Mueller <marcus.mueller@fau.de>
2023-01-23 18:11:35 +0100

    repeat runs

--- run_all_cp_target.sh
+++ run_all_cp_target.sh
@@ -7,5 +7,8 @@ cd ~/cp-target
 
 find . -name 'btree-*' -exec chmod u+x {}  \;
 
+for i in {1..20}
+do
 FILE="$HOME/data/urls" find . -name 'btree-*' -exec {} >> $OUT \;
 INT=2e7 find . -name 'btree-*' -exec {} >> $OUT \;
+done
