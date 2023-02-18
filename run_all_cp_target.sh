cafcd63ecd53c19c0e3828ea7f27ffe2f283bc45
Marcus Mueller <marcus.mueller@fau.de>
2023-02-18 19:23:12 +0100

    rerun hint aray comparison

--- run_all_cp_target.sh
+++ run_all_cp_target.sh
@@ -1,12 +1,15 @@
 set -e
 
-OUT="$HOME/adapt-leave-time.out"
+OUT="$HOME/hints-rerun.out"
 touch $OUT
 
 cd ~/cp-target
 
 find . -name 'btree-*' -exec chmod u+x {}  \;
 
+for i in {1..10}
+do
 OP_COUNT=1e8 INT=2E7 find . -name 'btree-*' -exec {} >> $OUT \;
 OP_COUNT=1e8 FILE="$HOME/data/urls" find . -name 'btree-*' -exec {} >> $OUT \;
+done
 
