4f5175ba583beb753d60972d4ac741c0e8701908
Marcus Mueller <marcus.mueller@fau.de>
2023-03-15 18:09:37 +0100

    cleanup

--- tpcc-run.sh
+++
@@ -1,6 +0,0 @@
-cd tpcc
-make
-rsync tpcc.elf zen4-students:
-ssh zen4-students 'env RUNFOR='$RUNFOR' ./tpcc.elf 1 '$WAREHOUSES &
-rsync tpcc.elf cascade-01:
-ssh cascade-01 'env RUNFOR='$RUNFOR' ./tpcc.elf 1 '$WAREHOUSES &
