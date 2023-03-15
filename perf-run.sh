09a8fce102144fa0f6a7fbd911d82d4b89c7062e
Marcus Mueller <marcus.mueller@fau.de>
2023-03-15 18:09:37 +0100

    cleanup

--- perf-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ssh cascade-01 rm -r btree
-cargo rustc --bin btree --release -- -C target-cpu=cascadelake
-rsync target/release/btree cascade-01:
-ssh -t cascade-01 OP_COUNT=1e8 INT=2e7 './btree'
